87964d7f59051f3094b2ded0d0b05db9d85afa21
JaroslavStrelkov <jaroslaw.strelkovsb@gmail.com>
2024-09-17 17:37:17 +0300

    commit Task_5 LOG

--- Task_5.cpp
+++ Task_5.cpp
@@ -5,4 +5,8 @@ std::cout<<"Введите значение х"<<std::endl;
 std::cin>> x;
 std::cout<<"Введите значение y"<<std::endl;
 std::cin>> y;
+if (x>y) {
+std::cout << x; }
+else {
+std::cout << y; }
 return 0; }
