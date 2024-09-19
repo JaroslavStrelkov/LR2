3a54cb7abaf2cc51d224471d2e4cbee5c05905d8
JaroslavStrelkov <jaroslaw.strelkovsb@gmail.com>
2024-09-19 15:25:25 +0300

    Task_6.cpp V1

--- Task_6.cpp
+++ Task_6.cpp
@@ -1,22 +1,16 @@
 #include <iostream>
 #include <cmath>
-int main() {
+int main(){
 setlocale (LC_ALL, "rus");    
-double Y, A, D, K, C, B, p, x;
+double S, A, D, b, x;
 std::cout <<"Введите значение числа D"<< std::endl;
 std::cin >>D;
-std::cout <<"Введите значение числа C"<< std::endl; 
-std::cin >>C;
-std::cout <<"Введите значение числа K"<< std::endl; 
-std::cin >>K;
 std::cout <<"Введите значение числа x"<< std::endl;
 std::cin >>x;
-std::cout <<"Введите значение числа p"<< std::endl;
-std::cin >>p;
-A = x + sin(p);
-B = exp(K);
-Y = 1 + ((K * K) / (2 * A * B)) - B + (D * C);
-std::cout <<Y;
+b = x + D;
+A = (D*x)/b;
+S = (A*A + b*cos(x))/(D*D*D + (A + D - b));
+std::cout <<S<< std::endl;
 return 0;
 }
 
