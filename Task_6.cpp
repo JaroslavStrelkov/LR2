915b2941a566eb8979a3b4a06650f078ef25fd18
JaroslavStrelkov <jaroslaw.strelkovsb@gmail.com>
2024-09-19 16:14:41 +0300

    Task_6.cpp V9

--- Task_6.cpp
+++ Task_6.cpp
@@ -2,18 +2,22 @@
 #include <cmath>
 int main(){
 setlocale (LC_ALL, "rus");    
-double A, K, C, B, Y, x, d;
+double A, K, C, D, B, Y, x, p, z;
 std::cout <<"Введите значение числа K"<< std::endl;
 std::cin >>K;
 std::cout <<"Введите значение числа C"<< std::endl;
 std::cin >>C;
-std::cout <<"Введите значение числа d"<< std::endl;
-std::cin >>d;
+std::cout <<"Введите значение числа D"<< std::endl;
+std::cin >>D;
 std::cout <<"Введите значение числа x"<< std::endl;
 std::cin >>x;
-A = log10(x);
-B = (x + exp(d));
-Y = ((A + B) - ((C * C) / K));
+std::cout <<"Введите значение числа p"<< std::endl;
+std::cin >>p;
+std::cout <<"Введите значение числа z"<< std::endl;
+std::cin >>z;
+A = (sin(x) - z);
+B = fabs(p - x);
+Y = (((A + B) * (A + B)) - (K / (C * D)));
 std::cout <<Y<< std::endl;
 return 0;
 }
