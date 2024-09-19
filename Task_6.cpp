0cfba6604966790fde4e800b4fe3c1b9ad285dbd
JaroslavStrelkov <jaroslaw.strelkovsb@gmail.com>
2024-09-19 15:43:19 +0300

    Task_6.cpp V6

--- Task_6.cpp
+++ Task_6.cpp
@@ -2,19 +2,19 @@
 #include <cmath>
 int main(){
 setlocale (LC_ALL, "rus");    
-double A, K, D, Y, n, m, x;
+double A, K, D, C, S, x, y;
 std::cout <<"Введите значение числа K"<< std::endl;
 std::cin >>K;
+std::cout <<"Введите значение числа C"<< std::endl;
+std::cin >>C;
 std::cout <<"Введите значение числа x"<< std::endl;
 std::cin >>x;
-std::cout <<"Введите значение числа n"<< std::endl;
-std::cin >>n;
-std::cout <<"Введите значение числа m"<< std::endl;
-std::cin >>m;
-A = fabs(n + m);
-D = tan(x);
-Y = (1.29 + (K / A) + (D * D));
-std::cout <<Y<< std::endl;
+std::cout <<"Введите значение числа y"<< std::endl;
+std::cin >>y;
+A = x + y;
+D = fabs(C - A);
+S = (10.1 + (A / C) + (D / (K * K)));
+std::cout <<S<< std::endl;
 return 0;
 }
 
