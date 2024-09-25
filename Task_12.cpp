49eb300291a6f651cb68d25ecce9f74e02fa9c8c
JaroslavStrelkov <jaroslaw.strelkovsb@gmail.com>
2024-09-26 00:30:06 +0300

    Commit 2

--- Task_12.cpp
+++ Task_12.cpp
@@ -1,33 +1,49 @@
 #include <iostream>
-double urav(double a, double b, double c, double x) {
-return a * x * x * x * x + b * x * x * x + c * x * x + b * x + a;
+double sqrt(double num) {
+if (num < 0) {
+return -1;
 }
-double proisvodn(double a, double b, double c, double x) {
-return 4 * a * x * x * x + 3 * b * x * x + 2 * c * x + b;
+double l = 0, r = num, mid;
+double epsilon = 1e-6;
+if (num < 1) r = 1;
+while (r- l > epsilon) {
+mid = (l + r) / 2;
+if (mid * mid < num) {
+l = mid; } 
+else {
+r = mid; }
 }
-void niuton(double a, double b, double c, double n1) {
-double x = n1; 
-double y = urav(a, b, c, x);
-double d;
-while (true) {
-d = proisvodn(a, b, c, x); 
-if (d == 0) {
-std::cout <<"Производная равна нулю." << std::endl;
-return; 
-}
-x = x - y / d;  
-y = urav(a, b, c, x);  
-if (y == 0) {  
-std::cout <<"Найден корень: x = " << x << std::endl;
-return;  
-}
-}
+return (l + r) / 2;
 }
 int main() {
-double a, b, c;
-std::cout <<"Введите значения a, b, c: ";
+double a, b, c, z, x1, x2, x3, x4, t1, t2, d, d2, d3;
+std::cout <<"Введите значения a, b, c: " << std::endl;
 std::cin >> a >> b >> c;
-double n1 = 0;  
-niuton(a, b, c, n1);
+z = (c - (2 * a));
+d = sqrt((b * b) - (4 * a * z));
+if (d < 0) {
+std::cout <<"Отрицательный дискриминант 1"<< std::endl;
+return 1;}
+else {
+t1 = ((-b - d) / (2  * a));
+t2 = ((-b + d) / (2  * a));}
+d2 = sqrt(t1 * t1 - 4);
+if (d2 < 0) {
+std::cout <<"Отрицательный дискриминант 2"<< std::endl;
+}
+else {
+x1 = ((t1 + d2) / 2);
+x2 = ((t1 - d2) / 2);
+std::cout << "x1: " << x1 << " x2: " << x2 << std::endl;}
+d3 = sqrt((t2 * t2) - 4);
+
+if (d3 < 0) {
+std::cout <<"Отрицательный дискриминант 3"<< std::endl;
+}
+else {
+x3 = ((t2 + d3) / 2);
+x4 = ((t2 - d3) / 2);
+std::cout << "x3: " << x3 << " x4 " << x4 << std::endl;
+}
 return 0;
 }
